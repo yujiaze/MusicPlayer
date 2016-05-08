e8a45fb52845b328775a0162687f03240c652547
yujiaze <136878608@qq.com>
2016-05-08 23:36:25 +0800

    add progress-bar, music-list and auto-play

--- js/app.js
+++ js/app.js
@@ -1,22 +1,27 @@
 (function() {
     'use strict';
     var app = {
-        inputDOM: document.getElementById('music-list'),
+        inputDOM: document.getElementById('music-input'),
         processedFiles: null,
         audioDOM: document.getElementById('audio'),
         playModeDOM: document.getElementById('play-mode'),
         playDOM: document.getElementById('play'),
         switchDOM: document.getElementById('switch-container'),
+        musicListDOM: document.getElementById('music-list'),
+        progressBarDOM: document.getElementById('progress-bar'),
         playMode: 'sequential',
         _current: 0, // current music playing in audio
         _isPlaying: false,
         init: function() { //some initial bindings
             this.inputDOM.addEventListener('change', this.handleInput.bind(app));
             this.switchDOM.addEventListener('click', this.handleSwitch.bind(app));
+            //forgery event parameter for this callback to acheive autoplay
+            this.audioDOM.addEventListener('ended', this.handleSwitch.bind(app, { target: { id: 'next' } }));
         },
         handleInput: function(e) {
             this.processedFiles = app.musicList.init(e.target.files);
             this.handlePlayMode(this.playMode);
+            this.renderMusicList();
         },
         handlePlayMode: function(playMode) {
             switch (playMode) {
@@ -30,6 +35,7 @@
             }
         },
         handlePlay: function(switching) {
+            this.renderProgressBar();
             if (switching) {
                 this._isPlaying = true;
                 this.audioDOM.play();
@@ -47,7 +53,7 @@
         },
         handleSwitch: function(e) {
             if (this.processedFiles) {
-            	var length = this.processedFiles.length;
+                var length = this.processedFiles.length;
                 if (e.target.id == 'next') {
                     if (this._current != --length) {
                         ++this._current;
@@ -81,7 +87,25 @@
             } else {
                 window.alert('请选择一个音乐文件夹');
             }
-        }
+        },
+        renderMusicList: function() {
+            var me = this;
+            this.musicListDOM.innerHTML = '';
+            this.processedFiles.forEach(function(music) {
+                var li = document.createElement('li');
+                li.id = music.mTitle;
+                li.innerHTML = music.mTitle + "<button>delete</button>"
+                me.musicListDOM.appendChild(li);
+            });
+        },
+        renderProgressBar: function() {
+            clearInterval(a);
+            var a = setInterval(function() {
+                this.progressBarDOM.children[0].style.left =
+                    (this.audioDOM.currentTime / this.audioDOM.duration) * 200 + 'px';
+            }.bind(this), '1000');
+        },
+
 
     }
     app.init();
